afc5684e132ed35d0beceb0e82eced29993295e7
juliamanska <julia.ma@onet.com.pl>
2023-11-16 12:34:51 +0100

    Add font styling to PDF

--- program-of-concert/src/Components/CreatePDF.tsx
+++ program-of-concert/src/Components/CreatePDF.tsx
@@ -1,4 +1,20 @@
-import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Font,
+} from "@react-pdf/renderer";
+import Roboto from "../assets/Roboto-Regular.ttf";
+import ItalicRoboto from "../assets/Roboto-Italic.ttf";
+import DancingScript from "../assets/DancingScript-Regular.ttf";
+
+Font.register({
+  family: "Arimo",
+  fonts: [{ src: Roboto }, { src: ItalicRoboto, fontStyle: "italic" }],
+});
+Font.register({ family: "Poppins", fonts: [{ src: DancingScript }] });
 
 const styles = StyleSheet.create({
   page: {
@@ -8,10 +24,33 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
   },
+  type: {
+    marginTop: 30,
+    fontSize: 30,
+    fontFamily: "Poppins",
+  },
+  date: {
+    fontFamily: "Arimo",
+    fontStyle: "italic",
+  },
+  teacher: {
+    fontFamily: "Arimo",
+  },
+  accompanist: {
+    fontFamily: "Arimo",
+  },
+  program: {
+    fontFamily: "Arimo",
+    fontStyle: "italic",
+  },
+  location: {
+    fontFamily: "Arimo",
+  },
 });
 
 interface SegmentProps {
   content: string;
+  style?: any;
 }
 
 const Segment = ({ content }: SegmentProps) => {
@@ -36,12 +75,24 @@ interface Props {
 const MyDocument = ({ dynamicContent }: Props) => (
   <Document>
     <Page size="A4" style={styles.page}>
-      <Segment content={`Przesłuchanie ${dynamicContent.type}`} />
-      <Segment content={`Data ${dynamicContent.date}`} />
-      <Segment content={`Nauczyciel prowadzący ${dynamicContent.teacher}`} />
-      <Segment content={`Akompaniator ${dynamicContent.accompanist}`} />
-      <Segment content={`Program ${dynamicContent.program}`} />
-      <Segment content={`Miejsce ${dynamicContent.location}`} />
+      <View style={styles.type}>
+        <Segment content={`Przesłuchanie ${dynamicContent.type}`} />
+      </View>
+      <View style={styles.date}>
+        <Segment content={`Data ${dynamicContent.date}`} />
+      </View>
+      <View style={styles.teacher}>
+        <Segment content={`Nauczyciel prowadzący ${dynamicContent.teacher}`} />
+      </View>
+      <View style={styles.accompanist}>
+        <Segment content={`Akompaniator ${dynamicContent.accompanist}`} />
+      </View>
+      <View style={styles.program}>
+        <Segment content={`Program ${dynamicContent.program}`} />
+      </View>
+      <View style={styles.location}>
+        <Segment content={`Miejsce ${dynamicContent.location}`} />
+      </View>
     </Page>
   </Document>
 );
