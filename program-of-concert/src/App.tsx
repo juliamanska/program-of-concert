f9f7eceef8581d3e541570978797212964f5d62c
juliamanska <julia.ma@onet.com.pl>
2023-11-17 12:58:43 +0100

    Change styling in Grid

--- program-of-concert/src/App.tsx
+++ program-of-concert/src/App.tsx
@@ -6,24 +6,41 @@ import SetDetails from "./Components/SetDetails";
 function App() {
   return (
     <Grid
-      templateAreas={`" header"
-                      " main"
-                      " footer"`}
-      // gridTemplateColumns={"1.5fr 3fr"}
-      gridTemplateRows={"45px 1fr 40px"}
-      h="800px"
+      templateAreas={`"header"
+                      "main"
+                      "footer"`}
+      gridTemplateRows={"80px 1fr 40px"}
+      minHeight="100vh"
       gap="5"
+      background="white"
     >
-      <GridItem color="white" area={"header"}>
+      <GridItem
+        color="#406163"
+        background="#CAE8E9"
+        area={"header"}
+        boxShadow="0 7px 10px 0px #BBC6C7"
+      >
         <Header />
       </GridItem>
-      <GridItem color="white" area={"main"}>
+      <GridItem
+        color="white"
+        area={"main"}
+        m=" 20px auto"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        background="#255052"
+        p="20px"
+        borderRadius="10px"
+      >
         <SetDetails onSubmit={() => console.log("submitted")} />
       </GridItem>
-      <GridItem color="black" bg="white" area={"footer"}>
-        Footer
-      </GridItem>
-      {/* <GridItem color="white" area={"aside"}></GridItem> */}
+      <GridItem
+        color="black"
+        bg="#CAE8E9"
+        area={"footer"}
+        boxShadow="0 -8px 10px 0px #BBC6C7"
+      ></GridItem>
     </Grid>
   );
 }
